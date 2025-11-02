eeb3ded5d8fec67351748e6f9fc45fe38ae22d35
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-11-02 15:16:06 +0600

    running

--- oc-zp-website/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  dataProvider: 'in-memory' as const,
-  apiUrl: '/api',
-};
--- oc-zp-website/src/environments/environment.ts
+++ oc-zp-website/src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: false,
-  dataProvider: 'in-memory' as const,
-  apiUrl: '/api',
+  apiUrl: 'http://localhost:5191',
 };
