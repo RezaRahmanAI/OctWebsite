4ce35c93d2d454db18a825d4c7e2edf296b454bc
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2025-11-22 21:02:09 +0600

    Restore navbar in application layout

--- frontend/src/app/features/layout/public-layout/public-layout.component.ts
+++ frontend/src/app/features/layout/public-layout/public-layout.component.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "../../../layout/footer/footer.component";import { NavbarComponent } from '../../../layout/navbar/navbar.component';
-
-
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './public-layout.component.html',
   styleUrls: ['./public-layout.component.css'],
 })
