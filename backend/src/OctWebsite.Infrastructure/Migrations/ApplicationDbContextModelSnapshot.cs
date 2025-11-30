971c49e75a0a79883c001cc80c8db22160dfbf05
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-11-30 16:30:02 +0600

    running

--- backend/src/OctWebsite.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ backend/src/OctWebsite.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace OctWebsite.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.8")
+                .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
